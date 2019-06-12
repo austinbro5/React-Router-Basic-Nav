5fe0a48f96f72baa1650295560c01c79d2c353ce
austin breaux <47146701+austinbro5@users.noreply.github.com>
2019-06-12 17:31:44 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import { Route } from 'react-router-dom';
 const App = () => (
   <div>
     <Navigation />
-    <Route exact path="/" compounent={Home}/>
-    <Route  exact  path="/About" compounent={About}/>
-    <Route path="/Contact" compounent={Contact}/>
+    <Route exact path="/" component={Home}/>
+    <Route  exact  path="/About" component={About}/>
+    <Route path="/Contact" component={Contact}/>
   </div>
 );
 
